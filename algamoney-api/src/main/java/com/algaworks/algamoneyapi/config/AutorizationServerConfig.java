3917e44c083a34f4eddfd0e544bb1c3f453d233f
pmattiollo <pmattiollo@gmail.com>
2017-11-22 14:32:59 -0200

    Adicionando perfis para a as configuracoes de seguranca

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/config/AutorizationServerConfig.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/config/AutorizationServerConfig.java
@@ -1,17 +1,25 @@
 package com.algaworks.algamoneyapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import com.algaworks.algamoneyapi.config.token.CustomTokenEnhancer;
+
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -30,8 +38,17 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-	endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter()).reuseRefreshTokens(false)
+	TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+	tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+
+	endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancerChain)
+		.accessTokenConverter(accessTokenConverter()).reuseRefreshTokens(false)
 		.authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public TokenEnhancer tokenEnhancer() {
+	return new CustomTokenEnhancer();
     }
 
     @Bean
