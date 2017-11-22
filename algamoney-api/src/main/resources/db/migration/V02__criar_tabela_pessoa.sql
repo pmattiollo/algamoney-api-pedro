fc30202fc7dddddf0fab888886cdb7962f68c9e6
pmattiollo <pmattiollo@gmail.com>
2017-11-21 23:03:57 -0200

    Correção na migração de dados versão 02

--- algamoney-api/src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ algamoney-api/src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -14,3 +14,5 @@ CREATE TABLE pessoa (
 INSERT INTO pessoa(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES('Pedro Humberto Mattiollo', 'Rua Guido Kaestner Sênior', '301', 'Apartamento 402', 'Boa Vista', '89012-360', 'Blumenau', 'SC', 1);
 INSERT INTO pessoa(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES('Dayanne Frizon Krindges', 'Rua Guido Kaestner Sênior', '301', 'Apartamento 402', 'Boa Vista', '89012-360', 'Blumenau', 'SC', 0);
 INSERT INTO pessoa(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES('Celso Mattiollo', 'Avenida XVII de Fevereiro', '408', 'Casa', 'Centro', '89745-970', 'Presidente Castelo Branco', 'SC', 1);
+INSERT INTO pessoa(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES('Marizete Savenhago Mattiollo', 'Avenida XVII de Fevereiro', '408', 'Casa', 'Centro', '89745-970', 'Presidente Castelo Branco', 'SC', 0);
+INSERT INTO pessoa(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) VALUES('Breno Mattiollo', 'Avenida XVII de Fevereiro', '408', 'Casa', 'Centro', '89745-970', 'Presidente Castelo Branco', 'SC', 1);
